6fddf50c04099cf8b3261df6e8060d5096a827c0
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-29 14:22:12 -0700

    folder structure, PrimaryUser.js started

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import lambda from './images/lambdalogo.png';
+import github from './images/githublogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={lambda} className="logos" alt="lambda logo" />
+        <img src={github} className="logos" alt="github logo" />
       </header>
+      <PrimaryUser/>
     </div>
   );
 }
---
+++ react-github-user-card/src/components/PrimaryUser.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PrimaryUser extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+        }
+    };
+
+    render(){
+        return(
+            
+        )
+    }
+
+};
+
+export default PrimaryUser;
